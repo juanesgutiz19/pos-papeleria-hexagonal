6c07e1d2cf6fa66ad342b223b11fa41f13b749b3
Juan Esteban Gutiérrez Zuluaga <juan.gutierrez@ceiba.com.co>
2023-10-12 19:16:23 -0500

    [FIX] Se agrega el método para reconstruir el objeto de Detalle Venta

--- microservicio/dominio/src/main/java/com/ceiba/papeleria/detalleventa/modelo/entidad/DetalleVenta.java
+++ microservicio/dominio/src/main/java/com/ceiba/papeleria/detalleventa/modelo/entidad/DetalleVenta.java
@@ -1,6 +1,7 @@
 package com.ceiba.papeleria.detalleventa.modelo.entidad;
 
 import com.ceiba.papeleria.articulo.modelo.entidad.Articulo;
+import com.ceiba.papeleria.dominio.ValidadorArgumento;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -16,5 +17,12 @@ public class DetalleVenta {
     private BigDecimal subtotal;
     private BigDecimal porcentajeDescuentoAplicadoDetalle;
 
-
+    private static DetalleVenta reconstruir(Long id, Articulo articulo, Integer cantidad, BigDecimal subtotal, BigDecimal porcentajeDescuentoAplicadoDetalle) {
+        ValidadorArgumento.validarObligatorio(id, "El id del detalle de la venta es obligatorio");
+        ValidadorArgumento.validarObligatorio(articulo, "El artículo asociado al detalle de la venta es obligatorio");
+        ValidadorArgumento.validarObligatorio(cantidad, "La cantidad asociada al detalle de la venta es obligatorio");
+        ValidadorArgumento.validarObligatorio(subtotal, "El subtotal del detalle de la venta es obligatorio");
+        ValidadorArgumento.validarObligatorio(porcentajeDescuentoAplicadoDetalle, "El porcentaje de cescuento aplicado del detalle de la venta es obligatorio");
+        return new DetalleVenta(id, articulo, cantidad, subtotal, porcentajeDescuentoAplicadoDetalle);
+    }
 }
